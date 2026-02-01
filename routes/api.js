7a0c012ca71f97b59a870cd9d3a51455d1aaf726
Nacho <irobertson@damsu.uncuyo.edu.ar>
2026-02-01 16:41:58 -0300

    Cambios menores previo subir heroku

--- routes/api.js
+++ routes/api.js
@@ -35,6 +35,7 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
 router.get('/novedades', async function(req, res, next) {
     let novedades = await novedadesModel.getNovedades();
@@ -59,6 +60,32 @@ router.get('/novedades', async function(req, res, next) {
     });
 
     res.json(novedades);
+});
+
+router.post('/reservas', async (req, res) => { // cambie /contacto por /reservas
+
+    const mail = {
+        to: 'juanignaciorobertson@gmail.com',
+        subject: 'Contacto desde la web',
+        html: `${req.body.nombre} se contacto a traves de la web y quiere mas informacion a este correo: ${req.body.email} 
+        <br> Ademas, hizo el siguiente comentario: ${req.body.mensaje} <br> Su telefono es: ${req.body.telefono}`
+    }
+
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    await transport.sendMail(mail);
+
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje enviado correctamente'
+    });
 });
 
 module.exports = router;
